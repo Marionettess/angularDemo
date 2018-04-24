de902646a021bc52614ab92d4b3828bac89742c5
Marionettess <1055394048@qq.com>
2018-04-24 09:53:25 +0800

    message

--- pages/message/message.ts
+++ pages/message/message.ts
@@ -17,16 +17,22 @@ import { MessinfPage } from '../messinf/messinf';
 export class MessagePage {
 
   messinfpage;
+  pname = ['资芽小助手','系统消息','张先生','张扬','李女士','东方资产管理'];
+  item = [0,1,2,3,4,5];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.messinfpage = MessinfPage;
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad MessagePage');
   }
-  pushMorePage(){
+  pushMorePage(e){
+    
+    console.log(this.pname[e]);
     console.log("代码方式跳转");
     //跳转到指定页面
-    this.navCtrl.push(MessinfPage);
+    this.navCtrl.push(MessinfPage,{
+      title : this.pname[e]
+    });
   }
 }
--- pages/messinf/messinf.ts
+++ pages/messinf/messinf.ts
@@ -14,10 +14,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'messinf.html',
 })
 export class MessinfPage {
-
+  title:string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    //获取传递过来的参数
+    this.title=this.navParams.get('title');
 
+    //当参数没有定义的情况
+    if(this.title == undefined){
+      this.title = "没有获取到结果";
+    }
+  }
+  pushHomePage(){
+    this.navCtrl.pop();
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MessinfPage');
   }
